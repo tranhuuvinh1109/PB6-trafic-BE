edf2e269c3a2de94724bbdb21fc69979b33539c3
kkoahis <khoakun27@gmail.com>
2023-12-28 15:27:16 +0700

    done unfix image to binary

--- request/util.py
+++ request/util.py
@@ -7,23 +7,8 @@ import imutils
 from PIL import Image
 
 def convert_to_binary(license_plate_crop):
-    # Chuyển ảnh sang ảnh xám
-    gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+    return license_plate_crop
 
-    # Áp dụng adaptive thresholding
-    T = threshold_local(gray, 15, offset=10, method="gaussian")
-    thresh = (gray > T).astype("uint8") * 255
-
-    # Chuyển pixel đen sang trắng và ngược lại
-    thresh = cv2.bitwise_not(thresh)
-    # thresh = imutils.resize(thresh, width=100)
-    thresh = cv2.resize(thresh,(100,100),interpolation=cv2.INTER_LANCZOS4) #or INTER_CUBIC
-    
-    # blurred = cv2.GaussianBlur(thresh, (5, 5), 0)
-    # sharpened = cv2.addWeighted(thresh, 2.0, blurred, -0.5, 0)
-    # resized = cv2.resize(sharpened, (150, 150))
-
-    return thresh
 
 
 def get_car(license_plate, vehicle_track_ids):
